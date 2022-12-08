b40b7416120275aefdfed4d1878b61bb95b28347
Jeffrey N. Carre <2999487+bleuscyther@users.noreply.github.com>
2022-12-07 20:10:01 -0500

    list + search

--- README.md
+++ README.md
@@ -1,61 +1,378 @@
 <p align="center">
- <img src="./wiki/banner.png" alt="" width="800">
+ <img src="./wiki/banner.png" alt="Showing the title of the repository, Country list , next to the Typsript logo and the fruitsBytes logo" width="800" title="Country list TS banner image">
 </p>
 
 # Country List TS
 
+This repository is based from the list of countries provided by [mledoze/countries](https://github.com/mledoze/countries). 
 
+It provides various compiled lists, mapped lists and tagged list of the property values. You can also search for a country based on the values in the property.
 
+>⚠ Warning: 
+>
+>Not all entities in this project are independent countries; refer to the independent property to know if the
+country is considered a sovereign state.
+
+## Features
+
+- Importable assets
+    - flags
+    - geo-json
+    - topo-json
+- Custom flag WebComponent
+- List
+    - of names, capitals, languages, currencies ...
+    - mapped lists by ISO 3166-1 alpha-2
+    - with
+- Search
 
 ## Installation
+
 ```shell
 npm i 
 ```
 
 ### Compatibility table
-| Version | JS target |  JS lib   |
-|:-------:|:---------:|:---------:|
-|  1.0.0  |    ES5    |  ES2017   |
+
+| Version | JS target | JS lib |
+|:-------:|:---------:|:------:|
+|  1.0.0  |    ES5    | ES2017 |
 
 ## Usage
 
 ### List
 
 Get list of country record member.
-- name
-- region
-- capital
-- continent
-- dialing_code
-- code
-- currency
-- provinces
 
-example:
-
+#### Simple list
 ```typescript
 import {ls} from "country-list-ts";
 
-const countryNames: string[] = ls('name');
-const capitals: string[] = ls('capital');
-const continents: string[] = ls('continent');
-```
+const c = ls('capital'); // ordered by country name
 
-Or using their alias:
+// Output:
+// ["Oranjestad","Kabul","Luanda","The Valley","Mariehamn","Tirana", ... ]
+````
+
+It's the same as :
 
 ```typescript
-import {names, capitals, continents} from "country-list-ts";
+import {capitals} from "country-list-ts";
 
-var countryNames: string[] = names();
-var capitals: string[] = capitals();
-var continents: string[] = continents();
+const c = capitals().sort(); // reorder alphabetically
+// Output: 
+// ['Abu Dhabi', 'Abuja', 'Accra', 'Adamstown', 'Addis Ababa', ... ]
 ```
+
+| function call           | alias                    |
+|-------------------------|--------------------------|
+| `ls("cca2")`            | `cca2s()`                |
+| `ls("tld") `            | `tlds()`                 |
+| `ls("ccn3")`            | `ccn3s() `               |
+| `ls("cca3")`            | `cca3s()`                |
+| `ls("cioc")`            | `ciocs()`                |
+| `ls("countryStatus")`   | `countryStatuses()`      |
+| `ls("commonName") `     | `commonNames()`          |
+| `ls("officialName") `   | `officialNames()`        |
+| `ls("currencyCode")`    | `currencyCodes()`        |
+| `ls("currencyName")`    | `currencyNames()`        |
+| `ls("currencySymbol")`  | `currencySymbols()`      |
+| `ls("region")`          | `regions()`              |
+| `ls("subRegion")`       | `subRegions()`           |
+| `ls("capital")`         | `capitals()`             |
+| `ls("lang")`            | `langs()`                |
+| `ls("languageName")`    | `languageNames()`        |
+
+
+#### Tagged lists
+```typescript
+import {lsF} from "country-list-ts";
+
+const currencies = lsF("currencies");
+
+// Output:
+// [
+//  { "currencies":{"AWG":{"name":"Aruban florin","symbol":"ƒ"}},"cca2":"AW"},
+//  { "currencies":{"AFN":{"name":"Afghan afghani","symbol":"؋"}},"cca2":"AF"},
+//   ...
+//]
+
+```
+Here is an example with Angular ngFor:
+````typescript
+@Component({
+  selector: 'app-select-country',
+  template: '<select> ' +
+          '<option *ngFor="let country of countryList" [ngValue]="country.cca2">' +
+          '{{country.name.official}}' +
+          '</option> ' +
+          '</select>',
+  styleUrls: ['./select-country.scss']
+})
+export class SelectCountryComponent {
+  public countryList = lsF(); // default list name
+}
+````
+
+#### Mapped list
+```typescript
+import {lsM} from "country-list-ts";
+const gps = lsM("latLng");
+// Output:
+// {"AW":[12.5,-69.96666666],"AF":[33,65],"AO":[-12.5,18.5],"AI":[18.25,-63.16666666],....]
+```
+
+Note, for tagged and mapped lists the available property values are:
+<p>
+name, tld, ccn3, cca3, cioc, independent, idd, countryStatus, currencies, region, subRegion, capitals, languages, translations, latLng, landlocked, borders, area, flag, demonyms, callingCodes
+</p>
+
+### All
+You can get a list of all the country data:
+
+```typescript
+import {all} from "country-list-ts";
+
+/**
+ * @var {Country[]}
+ */
+const countries = all();
+```
+
+An example of a Country object :
+
+```typescript
+const austria = {
+    name: {
+        common: "Austria",
+        official: "Republic of Austria",
+        native: {
+            bar: {
+                official: "Republik Österreich",
+                common: "Österreich"
+            }
+        }
+    },
+    tld: [".at"],
+    cca2: "AT",
+    ccn3: "040",
+    cca3: "AUT",
+    cioc: "AUT",
+    independent: true,
+    status: "officially-assigned",
+    unMember: true,
+    currencies: {
+        EUR: {
+            name: "Euro",
+            symbol: "€"
+        }
+    },
+    idd: {
+        "root": "+4",
+        "suffixes": ["3"]
+    },
+    capital: ["Vienna"],
+    altSpellings: ["AT", "Osterreich", "Oesterreich"],
+    region: "Europe",
+    subregion: "Western Europe",
+    languages: {
+        bar: "Austro-Bavarian German"
+    },
+    translations: {
+        cym: {official: "Republic of Austria", common: "Awstria"},
+        deu: {official: "Republik Österreich", common: "Österreich"},
+        fra: {official: "République d'Autriche", common: "Autriche"},
+        hrv: {official: "Republika Austrija", common: "Austrija"},
+        ita: {official: "Repubblica d'Austria", common: "Austria"},
+        jpn: {official: "オーストリア共和国", common: "オーストリア"},
+        nld: {official: "Republiek Oostenrijk", common: "Oostenrijk"},
+        por: {official: "República da Áustria", common: "Áustria"},
+        rus: {official: "Австрийская Республика", common: "Австрия"},
+        spa: {official: "República de Austria", common: "Austria"}
+    },
+    latlng: [47.33333333, 13.33333333],
+    demonyms: {
+        fra: {
+            f: "Autrichienne",
+            m: "Autrichien"
+        },
+        spa: {
+            f: "Austriaco",
+            m: "Austriaca"
+        }
+    },
+    landlocked: true,
+    borders: ["CZE", "DEU", "HUN", "ITA", "LIE", "SVK", "SVN", "CHE"],
+    area: 83871,
+    callingCodes: ["+43"],
+    flag: "🇦🇹"
+}
+```
+
+Definition:
+
+- `name`
+  - `common` - common name in english
+  - `official` - official name in english
+  - `native` - list of all native names
+    - key: three-letter ISO 639-3 language code
+    - value: name object
+      - key: official - official name translation
+      - key: common - common name translation
+- `tld` - country code top-level domain 
+- `cca2` - code ISO 3166-1 alpha-2
+- `cca3` - code ISO 3166-1 alpha-3
+- `ccn3` - code ISO 3166-1 numeric 
+- `cioc` - code International Olympic Committee
+- `independent` - ISO 3166-1 independence status  (denotes the country is considered a sovereign state)
+- `status` - ISO 3166-1 assignment status 
+- `unMember` - UN Member status
+- `currencies` - list of all currencies
+  - key: ISO 4217 currency code
+  - value: currency object
+    - key: `name` name of the currency
+    - key: `symbol` symbol of the currency
+-  `idd` - International Direct Dialing info 
+  - `root` - the root geographical code prefix. e.g. +6 for New Zealand, +4 for UK.
+  - `suffixes` - list of all suffixes assigned to this country. 4 for NZ, 809, 829, and 849 for Dominican Republic.
+- `capital` - capital city(ies)
+- `altSpellings` - alternative spellings 
+- `region`
+- `subregion`
+- `languages` - list of official languages
+  - key: three-letter ISO 639-3 language code
+  - value: name of the language in english
+- `translations` - list of name translations
+  - key: three-letter ISO 639-3 language code
+  - value: name object
+    - key: official - official name translation
+    - key: common - common name translation
+- `latlng` - latitude and longitude
+- `demonyms` - name of residents, translated & gendered
+  - key: three-letter ISO 639-3 language code
+  - value: genderized demonym object
+    - key: `f` (female) or `m` (male)
+    - value: genderized demonym translation
+- `landlocked` - landlocked status, if the country has direct access to ports for its maritime trade
+- `borders` - land borders
+- `area` - land area in km²
+- `flag` - Emoji flag
+- `callingCodes` - calling codes
 
 ### Search
 
+You can search for a country using the property name:
 
-### Dependencies
-Data Source [mledoze/countries](https://github.com/mledoze/countries).
+```typescript
+import {find} from "country-list-ts";
+
+/**
+ * @var {Country}
+ */
+const haiti = find("name.common", "Haiti") ;
+const {capital} = find("name.common", "Canada"); // ["Ottawa"]
+```
+The `find` frunction returns the first Country that satisfies the search value. To get a list use `get`
+
+```typescript
+import {get} from "country-list-ts";
+
+/**
+ * @var {Country[]}
+ */
+const tropicals = get("region", "Caribbean") ;
+```
+
+Both `find` and `get` are memoized;
+
+### Assets
+
+#### Flags
+
+#### Geo-JSON
+
+#### Topo-JSON
+
+## Extend
+For specific use case you can combine it with othe libraries:
+
+### Fuzzy search
+
+[Fuse.js](https://fusejs.io/) is a powerful, lightweight fuzzy-search library, with zero dependencies.
+
+Generally speaking, fuzzy searching (more formally known as approximate string matching) is the technique of finding strings that are approximately equal to a given pattern (rather than exactly
+
+You can use FuseJS for country list suggestion/TypeHead input
+
+````javascript
+import {all} from "country-list-ts";
+import Fuse from 'fuse.js';
+
+const countries = all();
+
+const fuse = new Fuse(countries, {
+  keys: ['cca2', 'cca3', 'name.common']
+})
+
+const found = fuse.search('hait');
+
+// Output:
+// [
+//   {
+//     item: {
+//        {
+//         ...
+//         "tld": [".ht"],
+//         "cca2": "HT",
+//         ...
+//        }
+//     },
+//     refIndex: 0
+//   },
+// ...
+// ]
+````
+
+
+More examples :
+
+- [Search String Array](https://fusejs.io/examples.html#search-string-array)
+- [Search Object Array](https://fusejs.io/examples.html#search-object-array)
+- [Nested Search - dot notation](https://fusejs.io/examples.html#nested-search)
+- [Weighted Search](https://fusejs.io/examples.html#weighted-search)
+- [Extended Search](https://fusejs.io/examples.html#extended-search)
+
+### Phone number
+[libphonenumber-js](https://www.npmjs.com/package/libphonenumber-js) is a simpler and smaller rewrite of Google Android's  libphonenumber library in javascript.
+
+See the [demo](https://catamphetamine.gitlab.io/libphonenumber-js/). 
+
+You can use this library to get the country of a phone number:
+
+```typescript
+// my-file.ts
+
+import parsePhoneNumber from 'libphonenumber-js';
+import {find} from "country-list-ts";
+
+function getCountryByPhonenumber(phone: string) {
+  const phoneNumber = parsePhoneNumber(phone);
+
+  if (phoneNumber) {
+    return find("cca2", phoneNumber.country);
+  }
+
+  return undefined;
+}
+
+/**
+ * @var {Country|undefined}
+ */
+const country = find("+50934524301");
+
+console.log(country?.name?.common, country?.flag); // Haiti 🇭🇹
+
+```
 
 ## Licence
 
--- src/index.ts
+++ src/index.ts
@@ -1,110 +1,398 @@
+/// <reference types="../globals" />
 
+// import * as _flags from "./data/flags";
+// import memoize from "fast-memoize";
+import {
+    AreaFatList,
+    AreaMap, BordersFatList,
+    BordersMap,
+    CallingCodesFatList,
+    CallingCodesMap, Capital,
+    CAPITAL_ARRAY, CapitalsFatList,
+    CapitalsMap,
+    CCA2, CCA3, CCA3FatList,
+    CCA3Map,
+    CCA_2_ARRAY,
+    CCA_3_ARRAY, CCN3, CCN3FatList,
+    CCN3Map,
+    CCN_3_ARRAY, CIOC,
+    CIOC_ARRAY, CIOCFatList,
+    CIOCMap,
+    COMMON_NAME_ARRAY, CommonName, countries, Country,
+    COUNTRY_STATUS_ARRAY, CountryStatus, CountryStatusFatList,
+    CountryStatusMap, CurrenciesFatList,
+    CurrenciesMap,
+    CURRENCY_CODE_ARRAY,
+    CURRENCY_NAME_ARRAY,
+    CURRENCY_SYMBOL_ARRAY, CurrencyCode, CurrencyName, CurrencySymbol,
+    DemonymsFatList,
+    DemonymsMap,
+    FlagsFatList,
+    FlagsMap,
+    IDDFatList,
+    IDDMap, IndependentFatList,
+    IndependentMap, LandlockedFatList,
+    LandlockedMap, Lang,
+    LANG_ARRAY,
+    LANGUAGE_NAME_ARRAY, LanguageName, LanguagesFatList,
+    LanguagesMap, LatLngFatList,
+    LatLngMap,
+    List,
+    Listable,
+    Mappable, NamesFatList,
+    NamesMap,
+    OFFICIAL_NAME_ARRAY, OfficialName, Region,
+    REGION_ARRAY,
+    RegionFatList,
+    RegionMap,
+    SUB_REGION_ARRAY, SubRegion, SubRegionFatList,
+    SubRegionMap, TLD,
+    TLD_ARRAY, TLDFatList,
+    TLDMap, TranslationsFatList,
+    TranslationsMap
+} from "./data/countries";
 import memoize from "fast-memoize";
-import {countries} from "./data/countries";
-countries
 
-export interface Country {
+/**
+ * ### List of string values
+ * @param {Listable} property - listable property
+ * @return {string[]} Array
+ */
+export function ls(property: Listable = "commonName"): List {
+    switch (property) {
+        case "cca2":
+            return CCA_2_ARRAY;
+        case "tld":
+            return TLD_ARRAY;
+        case "ccn3":
+            return CCN_3_ARRAY;
+        case "cca3":
+            return CCA_3_ARRAY;
+        case "cioc":
+            return CIOC_ARRAY;
+        case "countryStatus":
+            return COUNTRY_STATUS_ARRAY;
+        case "officialName":
+            return OFFICIAL_NAME_ARRAY;
+        case "currencyCode":
+            return CURRENCY_CODE_ARRAY;
+        case "currencyName":
+            return CURRENCY_NAME_ARRAY;
+        case "currencySymbol":
+            return CURRENCY_SYMBOL_ARRAY;
+        case "region":
+            return REGION_ARRAY;
+        case "subRegion":
+            return SUB_REGION_ARRAY;
+        case "capital":
+            return CAPITAL_ARRAY;
+        case "lang":
+            return LANG_ARRAY;
+        case "languageName":
+            return LANGUAGE_NAME_ARRAY;
+        case "default":
+        case "commonName":
+        default:
+            return COMMON_NAME_ARRAY;
 
+    }
 }
 
-
-
-
 /**
- * List of all countries
+ * List cca2 values
  */
-export const allArray = memoize(_allArray);
-
-function _ls(member: SearchableCountryRecordMember): Promise<Array<string | undefined>> {
-    return Promise.resolve(Object.keys(all()).map(k => this.all[k][member]))
+export function cca2s(): CCA2[] {
+    return ls("cca2");
 }
 
 /**
- * ### Get list of country record member.
- * example:
- *
- * ```typescript
- * const country_names = ls('name');
- * ```
- *
- * @param {SearchableCountryRecordMember} member
- * @return {Promise<Array<string | undefined>>}
+ * List tld values
  */
-export const ls = memoize(_ls);
-
-/**
- * Get list of country names
- */
-export function names(): Promise<string[]> {
-    return ls("name")
+export function tlds(): TLD[] {
+    return ls("tld");
 }
 
 /**
- * Get list of coutry continents
+ * List ccn3 values
  */
-export function continents(): Promise<string[]> {
-    return ls("continent");
+export function ccn3s(): CCN3[] {
+    return ls("ccn3");
 }
 
 /**
- * Get list of capitals
+ * List cca3 values
  */
-export function capitals(): Promise<string[]> {
+export function cca3s(): CCA3[] {
+    return ls("cca3");
+}
+
+/**
+ * List cioc values
+ */
+export function ciocs(): CIOC[] {
+    return ls("cioc");
+}
+
+/**
+ * List country status values
+ */
+export function countryStatuses(): CountryStatus[] {
+    return ls("countryStatus");
+}
+
+/**
+ * List common name values
+ */
+export function commonNames(): CommonName[] {
+    return ls("commonName");
+}
+
+/**
+ * List official name values
+ */
+export function officialNames(): OfficialName[] {
+    return ls("officialName");
+}
+
+/**
+ * List currency code values
+ */
+export function currencyCodes(): CurrencyCode[] {
+    return ls("currencyCode");
+}
+
+/**
+ * List currency name values
+ */
+export function currencyNames(): CurrencyName[] {
+    return ls("currencyName");
+}
+
+/**
+ * List currency symbol values
+ */
+export function currencySymbols(): CurrencySymbol[] {
+    return ls("currencySymbol");
+}
+
+/**
+ * List region values
+ */
+export function regions(): Region[] {
+    return ls("region");
+}
+
+/**
+ * List subRegion values
+ */
+export function subRegions(): SubRegion[] {
+    return ls("subRegion");
+}
+
+/**
+ * List capital values
+ */
+export function capitals(): Capital[] {
     return ls("capital");
 }
 
 /**
- * Get list of dialing codes
+ * List lang values
  */
-export function dialing_codes(): Promise<string[]> {
-    return ls("dialing_code")
+export function langs(): Lang[] {
+    return ls("lang");
+}
+
+/**
+ * List languageName values
+ */
+export function languageNames(): LanguageName[] {
+    return ls("languageName");
 }
 
 
 /**
+ * ### Mapped list of string values
  *
- * @param {string} iso2
+ * Example:
+ * ```
+ *  const topLevelDomains = lsM('tld');   // {'HT': ['.ht'], ...}
+ * ```
+ * @param {Mappable} property - mappable property
+ * @return {Record<string, any>} - a Record of values.
  */
-export function findByIso2(iso2: string): Country | undefined {
-    return all()[iso2];
-}
+export function lsM(property: Mappable = "name") {
+    switch (property) {
+        case "tld":
+            return TLDMap;
+        case "ccn3":
+            return CCN3Map;
+        case "cca3":
+            return CCA3Map;
+        case "cioc":
+            return CIOCMap;
+        case "independent":
+            return IndependentMap;
+        case "idd":
+            return IDDMap;
+        case "countryStatus":
+            return CountryStatusMap;
+        case "currencies":
+            return CurrenciesMap;
+        case "region":
+            return RegionMap;
+        case "subRegion":
+            return SubRegionMap;
+        case "capitals":
+            return CapitalsMap;
+        case "languages":
+            return LanguagesMap;
+        case "translations":
+            return TranslationsMap;
+        case "latLng":
+            return LatLngMap;
+        case "landlocked":
+            return LandlockedMap;
+        case "borders":
+            return BordersMap;
+        case "area":
+            return AreaMap;
+        case "flag":
+            return FlagsMap;
+        case "demonyms":
+            return DemonymsMap;
+        case "callingCodes":
+            return CallingCodesMap;
+        case "default":
+        case "name":
+        default:
+            return NamesMap;
 
-export function findByIso3(iso3: string): Country | undefined {
-    return allArray().find(({code: {iso3: i}}) => i === iso3);
-}
-
-export function findByName(name: string): Country | undefined {
-    return allArray().find(({name: n}) => n.normalize("NFD").replace(/\p{Diacritic}/gu, "") === name.normalize("NFD").replace(/\p{Diacritic}/gu, ""));
-}
-
-export function findByCapital(capital: string): Country | undefined {
-    return allArray().find(({capital: c}) => c.normalize("NFD").replace(/\p{Diacritic}/gu, "") === capital.normalize("NFD").replace(/\p{Diacritic}/gu, ""));
+    }
 }
 
 /**
- * Find country by currency
- * @param {string}currency
+ * ### Tagged list of string values
+ *
+ * Example:
+ * ```
+ *  const topLevelDomains = lsF('tld');   // [{tld: ['.ht'], cca2: 'HT'}, ...]
+ * ```
+ * @param {Mappable} property - mappable property
+ * @return {string[]} - array of objects.
  */
-export function findByCurrency(currency: string): Country | undefined {
-    return allArray().find(({currency: {code}}) => code === currency);
+export function lsF(property: Mappable = "name") {
+    switch (property) {
+        case "tld":
+            return TLDFatList;
+        case "ccn3":
+            return CCN3FatList;
+        case "cca3":
+            return CCA3FatList;
+        case "cioc":
+            return CIOCFatList;
+        case "independent":
+            return IndependentFatList;
+        case "idd":
+            return IDDFatList;
+        case "countryStatus":
+            return CountryStatusFatList;
+        case "currencies":
+            return CurrenciesFatList;
+        case "region":
+            return RegionFatList;
+        case "subRegion":
+            return SubRegionFatList;
+        case "capitals":
+            return CapitalsFatList;
+        case "languages":
+            return LanguagesFatList;
+        case "translations":
+            return TranslationsFatList;
+        case "latLng":
+            return LatLngFatList;
+        case "landlocked":
+            return LandlockedFatList;
+        case "borders":
+            return BordersFatList;
+        case "area":
+            return AreaFatList;
+        case "flag":
+            return FlagsFatList;
+        case "demonyms":
+            return DemonymsFatList;
+        case "callingCodes":
+            return CallingCodesFatList;
+        case "default":
+        case "name":
+        default:
+            return NamesFatList;
+
+    }
 }
 
 /**
- * Find country by province
- * @param {string}province
+ * ### Get a list of all the countries
+ * @return {Country[]} All the countries
  */
-export function findByProvince(province: string): Country | undefined {
-    return allArray().find(({provinces}) => {
+export function all(): Country[] {
+    return countries;
+}
 
-        const names = provinces.map(
-            (
-                {name, short}
-            ) => {
-                const p = province.normalize("NFD").replace(/\p{Diacritic}/gu, "");
-                return name.normalize("NFD").replace(/\p{Diacritic}/gu, "") === p ||
-                    short.normalize("NFD").replace(/\p{Diacritic}/gu, "") === p
+function _find(property: string, value: string): Country | undefined {
+
+    const c = property.split(".");
+
+    for (let country of countries) {
+        try {
+            if (property.split('.').reduce((o, i) => o[i], country) === value) {
+                return country;
             }
-        )
-    });
+        } catch (e) {
+            return undefined;
+        }
+    }
+
 }
 
+
+/**
+ * ### Find Countries
+ *
+ * example:
+ * ```javascript
+ * const haiti = find("name.common", "Haiti") ;
+ * const {capital} = find("name.common", "Canada"); // ["Ottawa"]
+ * ```
+ * @param {string} property Searchable property
+ * @param {string} value Property value
+ * @return {Country|undefined}
+ */
+export const find = memoize(_find);
+
+function _get(property: string, value: string): Array<Country>{
+    let result =[];
+    for (let country of countries) {
+        try {
+            if (property.split('.').reduce((o, i) => o[i], country) === value) {
+                result.push(country);
+            }
+        } catch (e) {
+           // do Nothing
+        }
+    }
+    return result
+}
+
+/**
+ * ### Search for Countries
+ *
+ * example:
+ * ```javascript
+ * const tropicals = get("region", "Caribbean") ;
+ * ```
+ * @param {string} property Searchable property
+ * @param {string} value Property value
+ * @return {Country[]}
+ */
+export const get = memoize(_get);
